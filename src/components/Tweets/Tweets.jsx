357d9391a8f23bc9afdd5d7570e61459d6f378bc
Faibysh <faybysh.b@gmail.com>
2023-05-09 13:42:43 +0200

    Try to fix build

---
+++ src/components/Tweets/Tweets.jsx
@@ -0,0 +1,13 @@
+// // import React from "react";
+// import Link from "react-router-dom";
+
+// export function Tweets() {
+//   return (
+//     <div>
+//       <h1>Welcome to Tweets page!</h1>
+//       <Link to="/">
+//         <button>Back to Home</button>
+//       </Link>
+//     </div>
+//   );
+// }
